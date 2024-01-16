3b362bcc31dcc4516fbf27b16cf3b941d273ee6f
Daniel Lifshitz <68195849+Danlif1@users.noreply.github.com>
2024-01-16 22:40:31 +0200

    MORE TESTS!!!!
    added more tests

--- tests/BloomFilter_test.cpp
+++ tests/BloomFilter_test.cpp
@@ -36,8 +36,10 @@ TEST(TruePositive, HashesTest) {
     BloomFilter filter = BloomFilter(256,{1,2,1,2,1,1,1,2});
     filter.add_url("www.example.com");
     // Check if added url appears on explicit urls blacklist.
-    EXPECT_EQ(filter.check_if_blacklisted("www.example.com1"), "false");
+    EXPECT_EQ(filter.check_if_blacklisted("www.example.com"), "true true");
 }
+
+
 TEST(FalsePositive, HashesTest) {
     BloomFilter filter = BloomFilter(5,{1,2,1,2,1,1,1,2});
     filter.add_url("www.example.com");
@@ -47,17 +49,60 @@ TEST(FalsePositive, HashesTest) {
     filter.add_url("www.example.com5");
     filter.add_url("www.example.com6");
 
-
-    // Check if added url appears on explicit urls blacklist.
     EXPECT_EQ(filter.check_if_blacklisted("www.example.com1"), "true false");
 }
 
-// Checking for true negative.
 TEST(TrueNegative, HashesTest) {
     BloomFilter filter = BloomFilter(256,{1,2,1,2,1,1,1,2});
     filter.add_url("www.example.com");
 //    // Check if added url appears on explicit urls blacklist.
     EXPECT_EQ(filter.check_if_blacklisted("www.example.com1"), "false");
 }
+
+// Checking for a lot of hash functions.
+TEST(TruePositive, LongTest) {
+    vector<int> funcs;
+    for (int i = 0; i < 100; i++) {
+        funcs.push_back(i%2 + 1);
+    }
+    BloomFilter filter = BloomFilter(3000,funcs);
+    for (int i = 0; i < 1000; i++) {
+        filter.add_url("www.example.com" + to_string(i));
+    }
+    // Check if added url appears on explicit urls blacklist.
+    for (int i = 0; i < 1000; i++) {
+        EXPECT_EQ(filter.check_if_blacklisted("www.example.com" + to_string(i)), "true true");
+    }
+}
+
+
+TEST(FalsePositive, LongTest) {
+    vector<int> funcs;
+    for (int i = 0; i < 100; i++) {
+        funcs.push_back(i%2 + 1);
+    }
+    BloomFilter filter = BloomFilter(1000,funcs);
+    for (int i = 0; i < 2000; i++) {
+        filter.add_url("www.example.com" + to_string(i));
+    }
+    for (int i = 2001; i < 3000; i++) {
+        EXPECT_EQ(filter.check_if_blacklisted("www.example.com" + to_string(i)), "true false");
+    }
+}
+
+TEST(TrueNegative, LongTest) {
+    vector<int> funcs;
+    for (int i = 0; i < 1000; i++) {
+        funcs.push_back(i%2 + 1);
+    }
+    BloomFilter filter = BloomFilter(300000,funcs);
+    for (int i = 0; i < 200; i++) {
+        filter.add_url("www.example.com" + to_string(i));
+    }
+    for (int i = 201; i < 301; i++) {
+        EXPECT_EQ(filter.check_if_blacklisted("www.example.com" + to_string(i)), "false");
+    }
+}
+
 
 // We can't check for a true negative with a lot of blacklisted urls in a static mode.
